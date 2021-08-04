2c541982ec0070e2e4ed0a6783fc724dea80c488
Luce Carter <luce@lucyscarter.co.uk>
2021-08-04 11:49:21 +0100

    WIP - Adding CRUD endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from "express";
+import * as GamesService from "./services/games.service"
+import { gamesRouter } from "./routes/games.router";
+
 const app = express();
 const port = 8080; // default port to listen
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.use("/games", gamesRouter); // send all calls to /games to our gamesRouter
 
 // start the Express server
 app.listen( port, () => {
-    // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
+    GamesService.initService();
 } );
