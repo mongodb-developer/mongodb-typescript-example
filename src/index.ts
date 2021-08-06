4820c0787714b4c309935a5c73552259658fc8e1
Luce Carter <luce@lucyscarter.co.uk>
2021-08-06 14:38:21 +0100

    Migrated to eslint

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
 // start the Express server
-app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${port}`);
+});
