73068ff3c36740bd34b08259a3e70b58c775e75c
Luce Carter <luce@lucyscarter.co.uk>
2021-08-09 14:16:40 +0100

    Added instruction comments to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
+
 const app = express();
 const port = 8080; // default port to listen
 
-// define a route handler for the default home page
+// ** TODO ** Replace this code with a call to your games router class to handle all calls to /games endpoint
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
 // start the Express server
-app.listen(port, () => {
-  // tslint:disable-next-line:no-console
+app.listen(port, () => { 
   console.log(`server started at http://localhost:${port}`);
+
+  // ** TODO ** Call to Game Service to initiate connection
 });
