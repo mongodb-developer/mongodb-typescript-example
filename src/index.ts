576adc86de39375a7fddc11e131beba7b3ed20c6
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2021-08-12 11:42:07 +0300

    refactor: extract db connection logic to a service

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import express from "express";
-import * as GamesService from "./services/games.service";
+import { connectToDatabase } from "./services/database.service";
 import { gamesRouter } from "./routes/games.router";
 
 const app = express();
 const port = 8080; // default port to listen
 
-app.use("/games", gamesRouter); // send all calls to /games to our gamesRouter
+connectToDatabase(app)
+    .then(() => {
+        // send all calls to /games to our gamesRouter
+        app.use("/games", gamesRouter);
 
-// start the Express server
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
-    // initService initiates the connection to our Atlas cluster
-    GamesService.initService();
-});
+        // start the Express server
+        app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);
+        });
+    })
+    .catch((error: Error) => {
+        console.error('Database connection failed', error);
+        process.exit();
+    });
--- src/services/games.service.ts
+++ src/services/games.service.ts
@@ -1,64 +1,9 @@
 import * as mongoDB from "mongodb";
-import * as dotenv from "dotenv";
 import Game from "../models/game";
-
-let client: mongoDB.MongoClient;
-let db: mongoDB.Db;
-let gamesCollection: mongoDB.Collection;
-
-export const initService = async () => {
-    // Pulls in the .env file so it can be accessed from process.env. No path as .env is in root, the default location
-    dotenv.config();
-
-    // Create a new MongoDB client with the connection string from .env
-    client = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
-
-    try {
-        // Connect to the cluster
-        await client.connect();
-        // Connect to the database with the name specified in .env
-        db = client.db(process.env.DB_NAME);
-        // Connect to the collection with the specific name from .env, found in the database previously specified
-        gamesCollection = db.collection(process.env.GAMES_COLLECTION_NAME);
-
-        // Update our existing collection with JSON schema validation so we know our documents will always match the shape of our Game model, even if added elsewhere.
-        // For more information about schema validation, see this blog series: https://www.mongodb.com/blog/post/json-schema-validation--locking-down-your-model-the-smart-way
-        await db.command({
-            collMod: process.env.GAMES_COLLECTION_NAME,
-            validator: {
-                $jsonSchema: {
-                    bsonType: "object",
-                    required: ["name", "price", "category"],
-                    additionalProperties: false,
-                    properties: {
-                        _id: {},
-                        name: {
-                            bsonType: "string",
-                            description: "'name' is required and is a string",
-                        },
-                        price: {
-                            bsonType: "number",
-                            description: "'price' is required and is a number",
-                        },
-                        category: {
-                            bsonType: "string",
-                            description: "'category' is required and is a string",
-                        },
-                    },
-                },
-            },
-        });
-
-        console.log(
-            `Successfully connected to database: ${db.databaseName} and collection: ${gamesCollection.collectionName}`,
-        );
-    } catch (ex) {
-        console.log(ex.message);
-    }
-};
+import { collections } from "./database.service";
 
 export const create = async (newGame: Game) => {
-    const result = await gamesCollection.insertOne(newGame);
+    const result = await collections.games.insertOne(newGame);
 
     if (result) {
         console.log(`New document added with name: ${newGame.name} and id: ${result.insertedId}`);
@@ -71,14 +16,14 @@ export const create = async (newGame: Game) => {
 
 export const readAll = async () => {
     // Call find with an empty filter object, meaning it returns all documents in the collection. Saves as Game array to take advantage of types
-    const games = (await gamesCollection.find({}).toArray()) as Game[];
+    const games = (await collections.games.find({}).toArray()) as Game[];
     return games;
 };
 
 export const read = async (id: string) => {
     // _id in MongoDB is an objectID type so we need to find our specific document by querying
     const query = { _id: new mongoDB.ObjectId(id) };
-    const result = (await gamesCollection.findOne(query)) as Game;
+    const result = (await collections.games.findOne(query)) as Game;
 
     return result;
 };
@@ -86,7 +31,7 @@ export const read = async (id: string) => {
 export const update = async (id: string, newGame: Game) => {
     const query = { _id: new mongoDB.ObjectId(id) };
     // $set adds or updates all fields
-    const result = await gamesCollection.updateOne(query, { $set: newGame });
+    const result = await collections.games.updateOne(query, { $set: newGame });
 
     if (result.modifiedCount > 0) {
         console.log(`Updated document ${id} - ${newGame.name}`);
@@ -99,7 +44,7 @@ export const update = async (id: string, newGame: Game) => {
 
 export const deleteOne = async (id: string) => {
     const query = { _id: new mongoDB.ObjectId(id) };
-    const result = await gamesCollection.deleteOne(query);
+    const result = await collections.games.deleteOne(query);
 
     if (result.deletedCount > 0) {
         console.log(`Deleted document ${id}`);
