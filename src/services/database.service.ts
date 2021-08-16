e1e1c84a00d915b0856760538d9170e6b7066dd8
Luce Carter <luce@lucyscarter.co.uk>
2021-08-16 15:18:33 +0100

    Added error handling around schema validation

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,6 +1,7 @@
 import { Express as ExpressApp } from "express";
 import * as mongoDB from "mongodb";
 import * as dotenv from "dotenv";
+import { env } from "process";
 
 export const collections: { games?: mongoDB.Collection } = {};
 
@@ -16,34 +17,10 @@ export async function connectToDatabase() {
 
     // Connect to the database with the name specified in .env
     const db: mongoDB.Db = client.db(process.env.DB_NAME);
+    
+    // Apply schema validation to the collection
+    await applySchemaValidation(db);
 
-    // Update our existing collection with JSON schema validation so we know our documents will always match the shape of our Game model, even if added elsewhere.
-    // For more information about schema validation, see this blog series: https://www.mongodb.com/blog/post/json-schema-validation--locking-down-your-model-the-smart-way
-    await db.command({
-        collMod: process.env.GAMES_COLLECTION_NAME,
-        validator: {
-            $jsonSchema: {
-                bsonType: "object",
-                required: ["name", "price", "category"],
-                additionalProperties: false,
-                properties: {
-                    _id: {},
-                    name: {
-                        bsonType: "string",
-                        description: "'name' is required and is a string",
-                    },
-                    price: {
-                        bsonType: "number",
-                        description: "'price' is required and is a number",
-                    },
-                    category: {
-                        bsonType: "string",
-                        description: "'category' is required and is a string",
-                    },
-                },
-            },
-        },
-    });
 
     // Connect to the collection with the specific name from .env, found in the database previously specified
     const gamesCollection: mongoDB.Collection = db.collection(process.env.GAMES_COLLECTION_NAME);
@@ -55,3 +32,42 @@ export async function connectToDatabase() {
         `Successfully connected to database: ${db.databaseName} and collection: ${gamesCollection.collectionName}`,
     );
 }
+// Update our existing collection with JSON schema validation so we know our documents will always match the shape of our Game model, even if added elsewhere.
+// For more information about schema validation, see this blog series: https://www.mongodb.com/blog/post/json-schema-validation--locking-down-your-model-the-smart-way
+async function applySchemaValidation(db: mongoDB.Db) {
+
+    const jsonSchema = {
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["name", "price", "category"],
+            additionalProperties: false,
+            properties: {
+                _id: {},
+                name: {
+                    bsonType: "string",
+                    description: "'name' is required and is a string",
+                },
+                price: {
+                    bsonType: "number",
+                    description: "'price' is required and is a number",
+                },
+                category: {
+                    bsonType: "string",
+                    description: "'category' is required and is a string",
+                },
+            },
+        },
+    };
+
+    // Try applying the modification to the collection, if the collection doesn't exist, create it 
+   await db.command({
+        collMod: process.env.GAMES_COLLECTION_NAME,
+        validator: jsonSchema
+    }).catch(async (error: mongoDB.MongoServerError) => {
+        if(error.codeName === 'NamespaceNotFound') {
+            await db.createCollection(process.env.GAMES_COLLECTION_NAME, {validator: jsonSchema});
+        }
+    });
+
+}
+
