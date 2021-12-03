d5d15092a54ca71f5bdf8e98d7537b26094b4591
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2021-12-03 11:45:35 +0200

    refactor: convert Class to an interface

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,8 @@
 import { ObjectId } from "mongodb";
 
-export default class Game {
-    constructor(public name: string, public price: number, public category: string, public id?: ObjectId) {}
+export default interface Game {
+    name: string;
+    price: number;
+    category: string;
+    id?: ObjectId;
 }
