cda0c0c35f3b0aad1e56e633dd9f0bfbec71bab1
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2021-12-07 16:28:31 +0200

    refactor: remove extraneous import

--- src/routes/games.router.ts
+++ src/routes/games.router.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
 import { ObjectId } from "mongodb";
 import { collections } from "../services/database.service";
-import Game from "../models/game";
 
 export const gamesRouter = express.Router();
 
