761b392297bf7a77bf000a4cebf196e69ae25f62
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-04-25 12:46:03 +0100

    ADD validation schema to database.service.ts

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -15,6 +15,32 @@ export async function connectToDatabase () {
         
     const db: mongoDB.Db = client.db(process.env.DB_NAME);
    
+    await db.command({
+        "collMod": process.env.GAMES_COLLECTION_NAME,
+        "validator": {
+            $jsonSchema: {
+                bsonType: "object",
+                required: ["name", "price", "category"],
+                additionalProperties: false,
+                properties: {
+                _id: {},
+                name: {
+                    bsonType: "string",
+                    description: "'name' is required and is a string"
+                },
+                price: {
+                    bsonType: "number",
+                    description: "'price' is required and is a number"
+                },
+                category: {
+                    bsonType: "string",
+                    description: "'category' is required and is a string"
+                }
+                }
+            }
+         }
+    });
+
     const gamesCollection: mongoDB.Collection = db.collection(process.env.GAMES_COLLECTION_NAME);
  
   collections.games = gamesCollection;
