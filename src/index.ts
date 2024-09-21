d4ee1e3f3e9ee3a6f38b2ea13c7c1f258610b932
John Lois Glifonea <jglifonea@sb2024.live>
2024-09-21 20:33:25 +0800

    try

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
-import express from "express";
-import { connectToDatabase } from "./services/database.service";
-import { gamesRouter } from "./routes/games.router";
+import { app } from '@azure/functions';
 
-const app = express();
-const port = 8080; // default port to listen
-
-connectToDatabase()
-    .then(() => {
-        // send all calls to /games to our gamesRouter
-        app.use("/games", gamesRouter);
-
-        // start the Express server
-        app.listen(port, () => {
-            console.log(`Server started at http://localhost:${port}`);
-        });
-    })
-    .catch((error: Error) => {
-        console.error("Database connection failed", error);
-        process.exit();
-    });
+app.setup({
+    enableHttpStream: true,
+});
